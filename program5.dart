59b45404bde5f2316303578d9673d29aa196af3e
Nandana <nandhnacb@gmail.com>
2024-07-19 14:26:28 +0530

    conditionalstatements on 19/7/2024

---
+++ program5.dart
@@ -0,0 +1,21 @@
+void main(){
+
+String weather="cloudy";
+switch(weather){
+
+case "sunny":
+   print("take sunscreem");
+   break;
+
+case "snowy":
+   print("its snow");
+   break;
+
+case "cloudy":
+   print("take umbrella");
+   break;
+
+default:print("invalid");
+break;
+}
+}
